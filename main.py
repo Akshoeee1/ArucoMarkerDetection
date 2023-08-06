de2d1278992f3042e90f793079f35c2878350277
Akshat Jain <124158255+Akshoeee1@users.noreply.github.com>
2023-08-06 23:16:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import numpy as np
 
 marker_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 
-param_markers = aruco.DetectorParameters_create()
+# used this dictionary as it is mentioned in the competition rules that they will display a black and white Aruco Tag using the 4x4_50 tag library on every marker
 
-# loading the sample image
+param_markers = aruco.DetectorParameters_create()
+# Loading the sample Image
 frame = cv.imread(r"C:\Users\Akshat\Downloads\arucoMarkerImage.png")
-# resizing the image
-frame = cv.resize(frame, (720, 720))
 
 gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 marker_corners, marker_IDs, reject = aruco.detectMarkers(
     gray_frame, marker_dict, parameters=param_markers
 )
+
 # the detectMarkers return 3 values
 # marker_corners: A list containing the corner coordinates of the detected markers.
 # marker_IDs: A list containing the IDs of the detected markers.
@@ -22,15 +22,18 @@ marker_corners, marker_IDs, reject = aruco.detectMarkers(
 
 if marker_corners:
     for ids, corners in zip(marker_IDs, marker_corners):
-        cv.polylines(
-            frame, [corners.astype(np.int32)], True, (0, 0, 255), 10, cv.LINE_AA
-        )
+        # Drawing the outline of the detected marker
+        cv.polylines(frame, [corners.astype(np.int32)], True, (0, 0, 255), 8, cv.LINE_AA)
+
+        # Reshaping and extracting corner points
         corners = corners.reshape(4, 2)
         corners = corners.astype(int)
         top_right = corners[0].ravel()
         top_left = corners[1].ravel()
         bottom_right = corners[2].ravel()
         bottom_left = corners[3].ravel()
+
+        # Drawing the ID of the marker at the top-right corner
         cv.putText(
             frame,
             f"id: {ids[0]}",
@@ -38,12 +41,21 @@ if marker_corners:
             cv.FONT_HERSHEY_COMPLEX,
             1.3,
             (200, 100, 0),
-            3,
+            5,
             cv.LINE_AA,
         )
+
+        # Printing the marker ID and its corner points
         print(ids, "  ", corners)
 
+# Create a resizable window and set it to full screen
+cv.namedWindow("frame", cv.WINDOW_NORMAL)
+cv.setWindowProperty("frame", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+
+# Display the image with detected markers
 cv.imshow("frame", frame)
+
+# Wait for a key press
 key = cv.waitKey(0)
 if key == ord("q"):
     cv.destroyAllWindows()
